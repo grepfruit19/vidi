6abcc6234d6458c3002855b94a253faa19d6044a
William Kim <wtk219@nyu.edu>
2017-02-26 16:31:31 -0500

    Lifted most of NewCards

--- src/modules/NewRoutine.js
+++ src/modules/NewRoutine.js
@@ -15,28 +15,73 @@ class NewRoutineContainer extends Component {
       routineDescription: '',
     }
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.addCard = this.addCard.bind(this);
   }
 
   //Ensures all form data is stored in state.
-  handleInputChange(event) {
+  handleInputChange(event, index) {
+    console.log(event, index);
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
+    //TODO: Figure out how to sort out state.name vs state.target.name
+    if (index!==undefined){
+      let temp = this.state.name.slice();
+      temp[index] = value;
+      this.setState({[name]: temp});
+    } else {
+      this.setState({
+        [name]: value
+      });
+    }
+  }
 
-    this.setState({
-      [name]: value
+  addCard(){
+    let temp = this.state.cards.slice();
+    let index = temp.length;
+    temp.push({
+      title: '',
+      description: '',
+      shortDescription: '',
+      duration: '',
+      days: '',
+      key: index,
+      index: index
     });
+    this.setState({cards: temp});
   }
 
   render(){
+    const cardList = this.state.cards.map((card) =>
+    <NewCard
+      key={card.key}
+      index={card.index}
+      left={this.state.left}
+      right={this.state.right}
+      title={card.title}
+      description={card.description}
+      shortDescription={card.shortDescription}
+      duration={card.duration}
+      days={card.days}
+      onChange={this.handleInputChange}
+      />
+    )
     return(
-      <NewRoutine
-        left={this.state.left}
-        right={this.state.right}
-        title={this.state.routineTitle}
-        timePeriod={this.state.routineTimePeriod}
-        description={this.state.routineDescription}
-        onChange={this.handleInputChange}/>
+      <div className="login">
+        <NewRoutine
+          left={this.state.left}
+          right={this.state.right}
+          title={this.state.routineTitle}
+          timePeriod={this.state.routineTimePeriod}
+          description={this.state.routineDescription}
+          onChange={this.handleInputChange}/>
+          <br/>
+          <br/>
+
+          {cardList}
+          <Button onClick={this.addCard} bsStyle="primary">Add Card</Button><br/>
+          <Button bsStyle="primary">Submit</Button>
+      </div>
     )
   }
 }
@@ -46,13 +91,6 @@ class NewRoutine extends Component {
   constructor(props){
     super(props);
     this.handleChange = this.handleChange.bind(this);
-  }
-
-  addCard(){
-    let temp = this.state.cards.slice();
-    let arraySize = temp.length;
-    temp.push(<NewCard key={arraySize}/>);
-    this.setState({cards: temp});
   }
 
   handleChange(event){
@@ -104,36 +142,20 @@ class NewRoutine extends Component {
           </Col>
         </FormGroup>
         {this.props.cards}
-        <Button onClick={this.addCard} bsStyle="primary">Add Card</Button><br/>
-        <Button bsStyle="primary">Submit</Button>
       </form>
     )
   }
 }
 
+//This class handles all cards associated with a routine.
 class NewCard extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      left: 2,
-      right: 10,
-      title: '',
-      description: '',
-      shortDescription: '',
-      duration: '',
-      days: '',
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
+  handleChange(event){
+    this.props.onChange(event, this.props.index);
   }
 
   render() {
@@ -141,21 +163,21 @@ class NewCard extends Component {
       <form className="innerForm">
         <h2>Create New Card</h2>
         <FormGroup controlId="cardTitle">
-          <Col componentClass={ControlLabel} sm={this.state.left}>
+          <Col componentClass={ControlLabel} sm={this.props.left}>
             Title
           </Col>
-          <Col sm={this.state.right}>
+          <Col sm={this.props.right}>
             <FormControl
               maxLength={30}
               type="text"
               name="title"
-              value={this.state.title}
-              onChange={this.handleInputChange}
+              value={this.props.title}
+              onChange={this.handleChange}
               placeholder="Card Title" />
           </Col>
         </FormGroup>
         <FormGroup controlId="cardDescription">
-          <Col componentClass={ControlLabel} sm={this.state.left}>
+          <Col componentClass={ControlLabel} sm={this.props.left}>
             Description
           </Col>
           <Col sm={10}>
@@ -163,69 +185,69 @@ class NewCard extends Component {
               maxLength={1000}
               type="text"
               name="description"
-              value={this.state.description}
-              onChange={this.handleInputChange}
+              value={this.props.description}
+              onChange={this.handleChange}
               placeholder="Description (Max 1000 chars)" />
           </Col>
         </FormGroup>
         <FormGroup controlId="cardShortDescription">
-          <Col componentClass={ControlLabel} sm={this.state.left}>
+          <Col componentClass={ControlLabel} sm={this.props.left}>
             Short Description
           </Col>
           <Col sm={10}>
             <FormControl
               maxLength={160}
               type="text"
-              value={this.state.shortDescription}
-              onChange={this.handleInputChange}
+              value={this.props.shortDescription}
+              onChange={this.handleChange}
               placeholder="An abbreviated description (Max 160 chars)" />
           </Col>
         </FormGroup>
         <FormGroup controlId="cardDays">
-          <Col componentClass={ControlLabel} sm={this.state.left}>
+          <Col componentClass={ControlLabel} sm={this.props.left}>
             Days
           </Col>
           <Col sm={10}>
             <FormControl
               type="text"
-              value={this.state.days}
-              onChange={this.handleInputChange}
+              value={this.props.days}
+              onChange={this.handleChange}
               placeholder="Input as list of numbers separated by commas (no spaces)" />
           </Col>
         </FormGroup>
         <FormGroup controlId="cardDuration">
-          <Col componentClass={ControlLabel} sm={this.state.left}>
+          <Col componentClass={ControlLabel} sm={this.props.left}>
             Duration
           </Col>
           <Col sm={10}>
             <FormControl
               type="number"
-              value={this.state.duration}
-              onChange={this.handleInputChange}
+              value={this.props.duration}
+              onChange={this.handleChange}
               placeholder="Duration (In number of minutes)"/>
           </Col>
         </FormGroup>
         <FormGroup controlId="cardVideoUrl">
-          <Col componentClass={ControlLabel} sm={this.state.left}>
+          <Col componentClass={ControlLabel} sm={this.props.left}>
             Video URL
           </Col>
           <Col sm={10}>
             <FormControl
               type="text"
-              value={this.state.videoUrl}
-              onChange={this.handleInputChange}
+              value={this.props.videoUrl}
+              onChange={this.handleChange}
               placeholder="Video URL"/>
           </Col>
         </FormGroup>
         <FormGroup controlId="cardOrder">
-          <Col componentClass={ControlLabel} sm={this.state.left}>
+          <Col componentClass={ControlLabel} sm={this.props.left}>
             Order
           </Col>
           <Col sm={10}>
             <FormControl
               type="number"
-              value={this.state.order}
-              onChange={this.handleInputChange}
+              value={this.props.order}
+              onChange={this.handleChange}
               placeholder="Order for any particular day" />
           </Col>
         </FormGroup>
