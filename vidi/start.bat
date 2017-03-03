6909a6943e6237af6c8d9551ea7273c0098f80ac
William Kim <wtk219@nyu.edu>
2017-03-03 11:25:47 -0500

    Authentication failed, trying new strat

--- vidi/start.bat
+++
@@ -1,3 +0,0 @@
-start cmd.exe @cmd /k "npm start"
-start cmd.exe @cmd /k "parse-dashboard --appId appid --masterKey mkey --serverURL "https://sentience.herokuapp.com/parse" --appName optionalName"
-atom
